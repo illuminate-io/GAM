11b336b2ff143c904f25b0470b815a5fc189c521
illuminate.io <isaac@hernandez.io>
2023-08-07 13:52:39 -0600

    renamed:    needs_conversion/Suspend Users in Bulk/Account Action - Suspend Students in Bulk.ps1 -> Suspend Users in Bulk/Account Action - Suspend Students in Bulk.sh 	renamed:    needs_conversion/Suspend Users in Bulk/suspend-students.csv -> Suspend Users in Bulk/suspend-students.csv

--- Suspend Users in Bulk/Account Action - Suspend Students in Bulk.sh
+++ Suspend Users in Bulk/Account Action - Suspend Students in Bulk.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+gam=/home/isaac/bin/gamadv-xtd3/gam
+
 gam csv suspend-students.csv gam update user ~email ou '/Users/Suspended Users/Withdrawn Students'
 gam csv suspend-students.csv gam user ~email delete groups
 gam csv suspend-students.csv gam update user ~email suspended on
 
-Write-Host "Completed Successfully."
+echo "Completed Successfully."
 
 pause
