4404b6bc84f9e8e668b94170236df269c6e43b07
illuminate.io <isaac@hernandez.io>
2023-08-08 15:00:55 -0600

    converted to bash

--- needs_conversion/Account Info - Get User Groups (for domain and non-domain emails).ps1
+++ needs_conversion/Account Info - Get User Groups (for domain and non-domain emails).ps1
@@ -1,10 +1,7 @@
-$UserEmail = Read-Host -Prompt 'Input the user email to get member groups'
-if ($UserEmail) {
-    Write-Host "Getting Groups info for [$UserEmail]"
-} else {
-    Write-Warning -Message "No user email input."
-}
+#!/bin/bash
 
-gam print groups domain santafeschool.org member | gam print group-members | Select-String $UserEmail
+gam=/home/isaac/bin/gamadv-xtd3/gam
 
-pause
+read -p "What is the email to remove?" UserEmail
+
+$gam print groups domain santafeschool.org member | $gam print group-members | grep $UserEmail
