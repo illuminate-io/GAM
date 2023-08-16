5777c6341ea870b3051b9095e30a6bad7f6b0b9e
isaac hernandez <isaac@hernandez.io>
2023-08-16 23:54:18 +0000

    changed create users script

--- CreateUsers.sh
+++ CreateUsers.sh
@@ -2,4 +2,4 @@
 
 gam=/home/isaac/bin/gamadv-xtd3/gam
 
-$gam redirect stdout CreateUsers.log multiprocess redirect stderr stdout csv CreateUsers.csv $gam create user "~useremail" firstname "~firstname" lastname "~lastname" ou "~ou" password "~password" notify "~~notifyemail~~" changepassword "~changepassword"
+$gam csv CreateUsers.csv gam create user "~useremail" firstname "~firstname" lastname "~lastname" ou "~ou" password "~password" notify "~~notifyemail~~" changepassword "~changepassword" >> createusers.log
