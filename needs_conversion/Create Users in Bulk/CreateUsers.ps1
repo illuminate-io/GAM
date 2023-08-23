85a5ff2d087eae148b6fcd31cbcfee1ca2d15420
Isaac <isaac@hernandez.io>
2023-08-22 21:01:22 -0600

    add excel file for new user creation

--- needs_conversion/Create Users in Bulk/CreateUsers.ps1
+++
@@ -1,1 +0,0 @@
-gam redirect stdout CreateUsers.log multiprocess redirect stderr stdout csv CreateUsers.csv gam create user "~useremail" firstname "~firstname" lastname "~lastname" ou "~ou" password "~password" notify "~~notifyemail~~" changepassword "~changepassword"
