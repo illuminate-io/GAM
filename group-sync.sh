87c99698a480422de693e569e2de9f98285a8ff4
illuminate-io <isaac@hernandez.io>
2024-07-09 14:29:09 -0600

    change back to sync+add

--- group-sync.sh
+++ group-sync.sh
@@ -170,8 +170,8 @@ STARGROUPS=("$groupSTARFAMstatic" "$group1FAMstatic" "$group2FAMstatic" "$groupS
 # Loop through each group and execute the command for managers that will receive mail
 for STARGROUP in "${STARGROUPS[@]}"; do
     echo "Updating group: $STARGROUP"
-    gam update group "$STARGROUP" sync managers csvfile "'gsheet:all-mail','gsheet:stargazer-mail'" gam@santafeschool.org "$GSHEET" "$MTAB"
-#    gam update group "$STARGROUP" add managers csvfile gsheet:stargazer-mail gam@santafeschool.org "$GSHEET" "$MTAB"
+    gam update group "$STARGROUP" sync managers csvfile gsheet:all-mail gam@santafeschool.org "$GSHEET" "$MTAB"
+    gam update group "$STARGROUP" add managers csvfile gsheet:stargazer-mail gam@santafeschool.org "$GSHEET" "$MTAB"
 #    gam update group "$STARGROUP" sync members csvfile gsheet:
 done
 
